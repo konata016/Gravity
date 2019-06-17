7f16597e0990786a9944426d0a7c006445a482fd
konata016 <konata016@gmail.com>
2019-06-17 21:46:33 +0900

    リフト追加

---
+++ Gravity/Assets/Script/Lift.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lift : MonoBehaviour
+{
+    Rigidbody rb;
+    Vector3 startPos;
+    bool isMoveEnd;
+    bool isSwitch;
+
+    public float speed;
+    public float goalPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //右に移動
+        if (transform.position.x <= startPos.x + goalPos && !isSwitch)
+        {
+            //慣性の消去
+            if (!isMoveEnd)
+            {
+                rb.isKinematic = true;
+                rb.isKinematic = false;
+                isMoveEnd = true;
+            }
+
+            rb.AddForce(speed * ((Vector3.right * speed) - rb.velocity), ForceMode.Acceleration);
+        }
+        else isSwitch = true;
+
+        //左に移動
+        if (transform.position.x >= startPos.x && isSwitch)
+        {
+            //慣性の消去
+            if (isMoveEnd)
+            {
+                rb.isKinematic = true;
+                rb.isKinematic = false;
+                isMoveEnd = false;
+            }
+
+            rb.AddForce(speed * ((Vector3.left * speed) - rb.velocity), ForceMode.Acceleration);
+        }
+        else isSwitch = false;
+    }
+}
